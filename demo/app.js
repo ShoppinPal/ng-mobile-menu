022f2a914ab3e1bb09ba8d06ef7f35568a89f2c1
Jeff French <jeff.french@moonswitch.com>
2013-07-17 14:30:39 -0500

    Update demo for v0.2.0

--- demo/app.js
+++ demo/app.js
@@ -1,1 +1,21 @@
-angular.module('super-awesome-demo',['shoppinpal.ng-mobile-menu']);
+angular.module('super-awesome-demo',['shoppinpal.mobile-menu'])
+    .config(['$routeProvider',function($routeProvider){
+        $routeProvider
+            .when("/skinny", {
+                templateUrl: "skinny.html"//,
+                //controller: "SkinnyCtrl"
+            })
+            .when("/wide", {
+                templateUrl: "wide.html"//,
+                //controller: "WideCtrl"
+            })
+            .otherwise({
+                redirectTo: "/skinny"
+            });
+    }]);
+//    .controller('SkinnyCtrl', ['$scope', '$routeParams', DummyCtrl])
+//    .controller('WideCtrl', ['$scope', '$routeParams', DummyCtrl]);
+//
+//function DummyCtrl($scope, $routeParams) {
+//
+//}
